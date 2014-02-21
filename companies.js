9eca02a7f5aecc4bd17cb5c60f09b4c4ef9d1647
Anniebunnie <aj.kim84@gmail.com>
2014-02-21 11:32:28 -0500

    all changes saved

---
+++ companies.js
@@ -0,0 +1,171 @@
+var companies = {
+ "name": "flare",
+ "children": [
+ {
+   "name": "analytics",
+   "children": [
+   {
+     "name": "social",
+     "children": [
+     {"name": "twitter", "size": 3000, "url" : "http://www.nytimes.com"},
+     {"name": "facebook", "size": 2383, "url" : "http://www.nytimes.com"},
+     {"name": "linkedin", "size": 17334, "url" : "http://www.nytimes.com"},
+     {"name": "pinterest", "size": 1998, "url" : "http://www.nytimes.com"}
+     ]
+   },
+   {
+     "name": "mobile",
+     "children": [
+     {"name": "instagram", "size": 3534, "url" : "http://www.nytimes.com"},
+     {"name": "twilio", "size": 5731, "url" : "http://www.nytimes.com"},
+     {"name": "sure", "size": 600, "url" : "http://www.nytimes.com"},
+     {"name": "ShortestPaths", "size": 2010, "url" : "http://www.nytimes.com"}
+     ]
+   },
+    {
+     "name": "category",
+     "children": [
+     {"name": "company", "size": 1208, "url" : "http://www.nytimes.com"},
+     {"name": "company", "size": 5731, "url" : "http://www.nytimes.com"},
+     {"name": "company", "size": 600, "url" : "http://www.nytimes.com"},
+     {"name": "company", "size": 2010, "url" : "http://www.nytimes.com"}
+     ]
+   },
+    {
+     "name": "ecommerce",
+     "children": [
+     {"name": "instagram", "size": 3534, "url" : "http://www.nytimes.com"},
+     {"name": "twilio", "size": 5731, "url" : "http://www.nytimes.com"},
+     {"name": "sure", "size": 600, "url" : "http://www.nytimes.com"},
+     {"name": "ShortestPaths", "size": 2010, "url" : "http://www.nytimes.com"}
+     ]
+   },
+    {
+     "name": "business",
+     "children": [
+     {"name": "instagram", "size": 3534, "url" : "http://www.nytimes.com"},
+     {"name": "twilio", "size": 5731, "url" : "http://www.nytimes.com"},
+     {"name": "sure", "size": 600, "url" : "http://www.nytimes.com"},
+     {"name": "ShortestPaths", "size": 2010, "url" : "http://www.nytimes.com"}
+     ]
+   },
+   ]
+ }
+ ]
+}
+// var companies = [
+// {
+// 	"name": "General Assembly",
+// 	"url": "http://www.google.com",
+// 	"category": "Mobile",
+// 	"total": "5,000,000",
+// 	"year": "2005"
+// },
+// {
+// 	"name": "General Assembly",
+// 	"url": "http://www.google.com",
+// 	"category": "Advertising",
+// 	"total": "5,000,000",
+// 	"year": "2005"
+// },
+// {
+// 	"name": "General Assembly",
+// 	"url": "http://www.google.com",
+// 	"category": "E-commerce",
+// 	"total": "5,000,000",
+// 	"year": "2005"
+// },
+// {
+// 	"name": "General Assembly",
+// 	"url": "http://www.google.com",
+// 	"category": "Analytics",
+// 	"total": "5,000,000",
+// 	"year": "2005"
+// },
+// {
+// 	"name": "General Assembly",
+// 	"url": "http://www.google.com",
+// 	"category": "Social",
+// 	"total": "5,000,000",
+// 	"year": "2005"
+// },
+// {
+// 	"name": "General Assembly",
+// 	"url": "http://www.google.com",
+// 	"category": "Software",
+// 	"total": "5,000,000",
+// 	"year": "2005"
+// },
+// {
+// 	"name": "General Assembly",
+// 	"url": "http://www.google.com",
+// 	"category": "Music",
+// 	"total": "5,000,000",
+// 	"year": "2005"
+// },
+// {
+// 	"name": "General Assembly",
+// 	"url": "http://www.google.com",
+// 	"category": "Social",
+// 	"total": "5,000,000",
+// 	"year": "2005"
+// },
+// {
+// 	"name": "General Assembly",
+// 	"url": "http://www.google.com",
+// 	"category": "Software",
+// 	"total": "5,000,000",
+// 	"year": "2005"
+// },
+// {
+// 	"name": "General Assembly",
+// 	"url": "http://www.google.com",
+// 	"category": "Mobile",
+// 	"total": "5,000,000",
+// 	"year": "2005"
+// },
+// {
+// 	"name": "General Assembly",
+// 	"url": "http://www.google.com",
+// 	"category": "Advertising",
+// 	"total": "5,000,000",
+// 	"year": "2005"
+// },
+// {
+// 	"name": "General Assembly",
+// 	"url": "http://www.google.com",
+// 	"category": "E-commerce",
+// 	"total": "5,000,000",
+// 	"year": "2005"
+// },
+// {
+// 	"name": "General Assembly",
+// 	"url": "http://www.google.com",
+// 	"category": "Analytics",
+// 	"total": "5,000,000",
+// 	"year": "2005"
+// },
+// {
+// 	"name": "General Assembly",
+// 	"url": "http://www.google.com",
+// 	"category": "Social",
+// 	"total": "5,000,000",
+// 	"year": "2005"
+// },
+// {
+// 	"name": "General Assembly",
+// 	"url": "http://www.google.com",
+// 	"category": "Software",
+// 	"total": "5,000,000",
+// 	"year": "2005"
+// },
+// {
+// 	"name": "General Assembly",
+// 	"url": "http://www.google.com",
+// 	"category": "Music",
+// 	"total": "5,000,000",
+// 	"year": "2005"
+// }
+
+// ]
+
